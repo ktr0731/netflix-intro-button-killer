78ca24ed4d39445b3c267634295c7338625a6ed0
ktr0731 <aizu.s1230022@gmail.com>
2018-05-14 01:38:10 +0900

    fix

--- extension/script.js
+++ extension/script.js
@@ -1,6 +1,6 @@
 new MutationObserver(() => {
-    const e = document.querySelector(".VideoContainer");
-    e && e.click();
+    const e = document.querySelector(".ptrack-container");
+    e && document.querySelector(".VideoContainer").click();
 }).observe(document.querySelector("#appMountPoint"), {
     childList: true,
     subtree: true
